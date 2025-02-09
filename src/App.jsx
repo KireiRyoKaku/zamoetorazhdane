d3e83bb0ed32e561df27316a4cd051eb907bd44f
Passenger <cls1337@gmail.com>
2025-02-09 01:34:25 +0100

    swaps with HashRouter as per recommendation for GitHubPages

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,14 @@ import TitleAndMenu from "./TitleAndMenu";
 import Events from "./Events";
 import About from "./About";
 import SubscribeAndPay from "./SubscribeAndPay";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  // Get the base path from Vite's environment
-  const basePath =
-    import.meta.env.MODE === "production" ? "/zamoetorazhdane" : "";
-
   return (
     <>
       <div className="flex min-h-screen flex-col justify-center text-center">
         <Background />
-        <Router basename={basePath}>
+        <Router>
           <Routes>
             <Route path="/" element={<TitleAndMenu />} />
             <Route path="/about" element={<About />} />
