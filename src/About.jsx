1b72ee42efe4fe7a1c1457f59b618426d5607213
Passenger <cls1337@gmail.com>
2025-09-06 13:13:24 +0200

    updateing UX on the about -page

--- src/About.jsx
+++ src/About.jsx
@@ -71,15 +71,15 @@ const About = ({ onClose = () => {} }) => {
 
   return (
     <div className="flex flex-col items-center">
-      <div className="fixed z-50 mt-7 flex items-center justify-center px-5 md:inset-0 md:-mt-14 md:px-10">
+      <div className="fixed inset-0 z-50 flex items-center justify-center px-5 pb-20 md:px-10 md:pb-0">
         {/* Updated spacing with negative margin instead of padding */}
         <div
-          className={`h-[calc(100dvh-125px)] w-full max-w-3xl transform overflow-y-auto rounded-lg bg-slate-50 p-8 text-left font-yanoneKaffeesatz text-xl font-light transition-all duration-300 ease-in-out md:h-[calc(100dvh-150px)] ${
+          className={`max-h-[85vh] w-full max-w-3xl transform overflow-y-auto rounded-lg bg-slate-50 p-8 text-left font-rocaTwoThin text-xl font-light transition-all duration-300 ease-in-out ${
             isVisible ? "translate-y-0 opacity-100" : "translate-y-4 opacity-0"
           }`}
           ref={contentRef}
         >
-          <div className="mb-1 -mt-5 flex justify-center">
+          <div className="-mt-5 mb-1 flex justify-center">
             <img
               src={logoImage}
               alt="Прегърната лого"
@@ -89,7 +89,7 @@ const About = ({ onClose = () => {} }) => {
           <div className="relative">
             <h3>
               <div className="mb-5 flex justify-center">
-                <span className="font-magnoliaScript text-2xl md:text-3xl lg:text-6xl">
+                <span className="font-magnoliaScript text-4xl md:text-5xl lg:text-6xl">
                   Прегърната
                 </span>
               </div>
@@ -97,7 +97,9 @@ const About = ({ onClose = () => {} }) => {
                 ref={(el) => (elementsRef.current[0] = el)}
                 className="para1 opacity-0 transition-opacity duration-1000 ease-in-out"
               >
-                 е общност, създадена от и за майки. Място, в което на фокус е жената – приета, чута и подкрепена във всеки етап на своето майчинство.
+                е общност, създадена от и за майки. Място, в което на фокус е
+                жената – приета, чута и подкрепена във всеки етап на своето
+                майчинство.
               </p>
 
               <br />
@@ -105,31 +107,37 @@ const About = ({ onClose = () => {} }) => {
                 ref={(el) => (elementsRef.current[2] = el)}
                 className="para2 opacity-0 transition-opacity duration-1000 ease-in-out"
               >
-                Нашата мисия е да създадем пространство, в което майки се събират и взаимодействат помежду си въз основа на <b>своите ценности и интереси в и извън родителството.</b>
+                Нашата мисия е да създадем пространство, в което майки се
+                събират и взаимодействат помежду си въз основа на{" "}
+                <b>своите ценности и интереси в и извън родителството.</b>
               </p>
               <br />
               <p
                 ref={(el) => (elementsRef.current[3] = el)}
                 className="para3 opacity-0 transition-opacity duration-1000 ease-in-out"
               >
-                Тук <b>всяка майка има право на подкрепа</b>, разбиране и позволение за собствените си емоции и нужди. Силно вярваме в това, че <b>когато тя е</b> <span className="font-magnoliaScript">„прегърната“</span>, може с повече лекота и радост да бъде до своите деца и семейство.
+                Тук <b>всяка майка има право на подкрепа</b>, разбиране и
+                позволение за собствените си емоции и нужди. Силно вярваме в
+                това, че <b>когато тя е</b>{" "}
+                <span className="font-magnoliaScript">„прегърната“</span>, може
+                с повече лекота и радост да бъде до своите деца и семейство.
                 <br />
               </p>
             </h3>
-              <div
-                ref={(el) => (elementsRef.current[1] = el)}
-                className="mt-8 flex justify-center opacity-0 transition-opacity duration-1000 ease-in-out"
+            <div
+              ref={(el) => (elementsRef.current[1] = el)}
+              className="mt-6 flex justify-center opacity-0 transition-opacity duration-1000 ease-in-out"
+            >
+              <button
+                className="rounded-3xl bg-moetoRazhdaneYellow px-6 py-2 font-rocaTwoBold text-2xl font-black text-black transition-all duration-500 ease-in-out hover:bg-transparent hover:text-black/30"
+                onClick={handleClickEventTeam}
               >
-                <button
-                  className="rounded-3xl bg-moetoRazhdaneYellow px-6 py-2 font-rocaTwoBold text-2xl font-black text-black transition-all duration-500 ease-in-out hover:bg-transparent hover:text-black/30"
-                  onClick={handleClickEventTeam}
-                >
-                  ЗАПОЗНАЙ СЕ С ЕКИПА
-                </button>
-              </div>
+                ЗАПОЗНАЙ СЕ С ЕКИПА
+              </button>
+            </div>
             <div
               ref={(el) => (elementsRef.current[4] = el)}
-              className="mt-8 flex justify-center opacity-0 transition-opacity duration-1000 ease-in-out"
+              className="mt-4 flex justify-center opacity-0 transition-opacity duration-1000 ease-in-out"
             >
               <button
                 className="rounded-3xl bg-moetoRazhdaneYellow px-6 py-2 font-rocaTwoBold text-2xl font-black text-black transition-all duration-500 ease-in-out hover:bg-transparent hover:text-black/30"
